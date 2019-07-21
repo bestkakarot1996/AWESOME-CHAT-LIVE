d32fe4a44bc1f5e29eff3e114f88ebcd67369b51
bestkakarot1996 <dangtrieuvi18091996@gmail.com>
2019-07-22 01:13:30 +0700

    connect database table and create process env

---
+++ src/connectBD.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import bluebird from "bluebird";
+require('dotenv').config();
+/** 
+CONNECT TO DATABASE MONGODB
+*/
+let connectDB = () => {
+  mongoose.Promise =  bluebird;
+  // mongodb://localhost:27017/awsome_chat
+  let URL = `${process.env.DB_CONNECT}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  console.log(URL);
+  // sử dụng thuộc tính connect trong mongo để kết nối đến database
+  return mongoose.connect(URL, {useMongoClient: true}); 
+}
+
+module.exports = connectDB;
