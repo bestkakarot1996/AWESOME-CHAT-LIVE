a564371c0ca5445844cf39c07512f22cd2d26758
bestkakarot1996 <dangtrieuvi18091996@gmail.com>
2019-08-01 21:10:06 +0700

    add file layout app

---
+++ src/config/viewEngine.js
@@ -0,0 +1,14 @@
+import express from "express";
+import ExpressEjsExtend from "express-ejs-extend";
+
+/**
+ Cấu hình config viewEngine 
+ */
+let configViewEngine = (app) => {
+app.use(express.static("./src/public"));// file này dung để lưu các file (thư viện) js , ejs,.. về fordel public
+app.engine("ejs", ExpressEjsExtend );
+app.set("view engine","ejs"); // sau khi cấu hình thì set view engine là ejs 
+app.set("views", "./src/views"); // đường dẫn đến thư mục chứa giao diện website
+}
+
+module.exports = configViewEngine;
