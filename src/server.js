53985f33cc18d9b7aca34e86d0221287c03bd19e
bestkakarot1996 <dangtrieuvi18091996@gmail.com>
2019-08-23 19:44:44 +0700

    update avatar frontend

--- src/server.js
+++ src/server.js
@@ -13,42 +13,9 @@ import configSession from "./config/session";
 // import passport
 require('dotenv').config();
 import passport from "passport";
-import pem from "pem";
-import https from "https";
-// import dotenv from "dotenv";
-// config openssl for windown
-pem.config({
-  pathOpenSSL: 'C:\\Program Files\\OpenSSL-Win64\\bin\\openssl'
-})
+// import pem from "pem";
+// import https from "https";
 
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-  if (err) {
-    throw err
-  }
-  let app = express();
- 
-  // run function connectDB MongoDB
-  connectDB();
-  // run configSession after run file connectDB
-  configSession(app);
-  // configViewEngin 
-  configViewEngine(app);
-  // enable bodyParser app 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  // enable connectFlash
-  app.use(connectFlash());
-  // config Passprort
-  app.use(passport.initialize());
-  app.use(passport.session());
-  // initRoutes all router
-  initRoutes(app);
-
-  https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(process.env.APP_PORT, process.env.APP_HOST, () => {
-    console.log(`my name app message chat $(hostname):${process.env.APP_PORT}/`);
-  });
-})
-
-/**
  
  let app = express();
  
@@ -71,4 +38,42 @@ pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
    console.log(`my name app message chat $(hostname):${process.env.APP_PORT}/`);
  });
 
- */
+
+ /** -----------------------------------------PEM SSL ---------------------------------------------- */
+
+
+
+// import dotenv from "dotenv";
+// config openssl for windown
+// pem.config({
+//   pathOpenSSL: 'C:\\Program Files\\OpenSSL-Win64\\bin\\openssl'
+// })
+
+// pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+//   if (err) {
+//     throw err
+//   }
+//   let app = express();
+ 
+//   // run function connectDB MongoDB
+//   connectDB();
+//   // run configSession after run file connectDB
+//   configSession(app);
+//   // configViewEngin 
+//   configViewEngine(app);
+//   // enable bodyParser app 
+//   app.use(bodyParser.urlencoded({ extended: true }));
+//   // enable connectFlash
+//   app.use(connectFlash());
+//   // config Passprort
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+//   // initRoutes all router
+//   initRoutes(app);
+
+//   https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+//     console.log(`my name app message chat $(hostname):${process.env.APP_PORT}/`);
+//   });
+// })
+
+
